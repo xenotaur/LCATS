680edac64e55be82699d9ee77528fc6f210d21d7
Anthony Francis <centaur@logicalrobotics.com>
2026-02-22 12:08:36 -0500

    Nondeterministic encoding fix.

--- lcats/tests/gatherers_tests/downloaders_test.py
+++ lcats/tests/gatherers_tests/downloaders_test.py
@@ -58,13 +58,13 @@ class TestDetectEncoding(unittest.TestCase):
         enc = downloaders.detect_encoding(data)
         # These cannot be reliably distinguished at the byte level as
         # both encodings are valid for the same byte sequence.
-        self.assertIn(enc.upper(), {"ISO-8859-1", "WINDOWS-1252"})
+        self.assertIn(enc.upper(), {"ISO-8859-1", "WINDOWS-1250", "WINDOWS-1252"})
 
     def test_detect_encoding_cp1252_distinguishing(self):
         # 0x80 is € in cp1252, control in iso-8859-1
         data = b"Price: \x80 10"
         enc = downloaders.detect_encoding(data)
-        self.assertEqual(enc.upper(), "WINDOWS-1252")
+        self.assertIn(enc.upper(), {"WINDOWS-1250", "WINDOWS-1252"})
 
     def test_detect_encoding_ascii(self):
         """Test detecting ASCII encoding."""
